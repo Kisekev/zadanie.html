f0381c3fd3c7786ed8e73c6b0b2d6c438c5db4c6
Kisekev <130700381+Kisekev@users.noreply.github.com>
2025-06-10 21:58:16 +0300

    Add files via upload

---
+++ Project/index.js
@@ -0,0 +1,15 @@
+//console.log('JavaScript-код с созданием переменных');
+/*
+console.log('Автор');
+console.log('Вася Петечкин');
+*/
+var myName = "Вася";
+console.log(myName);
+let mySurname = "Петечкин";
+const myFavoriteDrink = "Чай";
+var myFavoriteAnimal = "Собака";
+let myFavoriteProgrammingLanguage = "JavaScript";
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
